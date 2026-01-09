5352e973dd393d54b76f0827827947163958b3ab
Coderzz21 <rdm112005@gmail.com>
2026-01-09 20:31:27 +0530

    Add files via upload

--- Frontend/src/components/Chat.js
+++ Frontend/src/components/Chat.js
@@ -3,7 +3,7 @@ import MessageBubble from './MessageBubble';
 import EmojiPickerComponent from './EmojiPickerComponent';
 import FileUpload from './FileUpload';
 import ImageModal from './ImageModal';
-import { formatDateIST } from '../utils/timeUtils';
+import { formatDateIST, formatLastSeen } from '../utils/timeUtils';
 import './Chat.css';
 
 function Chat({ socket, currentUser, selectedUser, onlineUsers, backendUrl, onBack, isMobileView }) {
